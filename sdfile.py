3f48833d6321533b8264f36e313db83c17e72794
sarthakdarji <sarthak.darji@auberginesolutions.com>
2022-01-05 17:26:14 +0530

    water

--- sdfile.py
+++ sdfile.py
@@ -2,4 +2,8 @@ x=input("Are you a student?")
 if x=="yes":
     print("All the best")
 else:
+    thirsty=input("Are you thirsty?")
+    if thirsty=="yes":
+        print("Drink Water")
+        
     print("Have a good day")
