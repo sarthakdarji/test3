3a2958b5473e2743c2d664d0f4b0dee156937160
sarthakdarji <sarthak.darji@auberginesolutions.com>
2022-01-06 11:11:51 +0530

    Extra instruction

--- sdfile.py
+++ sdfile.py
@@ -1,6 +1,8 @@
 x=input("Are you a student?")
 if x=="yes":
     print("All the best")
+    print("Take care of yourself")
+    print("Be sincere")
 else:
     thirsty=input("Are you thirsty?")
     if thirsty=="yes":
